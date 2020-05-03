9094e69c23a068f4110f9464e83c1cd19bf8ff9c
github username <github email address>
2020-05-03 13:42:07 -0700

    completed contorllers

--- ApplicationApp_backend/app/controllers/notes_controller.rb
+++ ApplicationApp_backend/app/controllers/notes_controller.rb
@@ -1,2 +1,44 @@
 class NotesController < ApplicationController
+    before_action :set_note, only: [:show, :update, :destroy]
+    
+    def show
+        render json: @note, include: ['user','stages', 'notes', 'tasks'] 
+    end 
+
+    def create
+        note = note.new(note_params)
+        if note.save
+            render json:note
+        # else
+        #     flash[:error_messages] = note.errors.full_messages  
+        #     render json: flash[:error_messages] 
+        end 
+        
+    end 
+
+    def update
+        if @note.update(note_params)
+            render json:@note
+        # else
+        #     flash[:error_messages] = @note.errors.full_messages  
+        #     render json: flash[:error_messages] 
+        end 
+        
+    end 
+    
+    def destroy 
+        @note.destroy
+        render json:"note Deleted"
+    end 
+
+    private 
+
+    def set_note
+        @note = note.find(params[:id])
+    end
+
+    def note_params
+        params.require(:note).permit(:id, :title, :detials, :stage_id )
+    end
+
 end
